6cd8c00043523b1cb683847166b9b471677e3814
girasenandu <girasenandu@gmail.com>
2026-09-17 23:01:32 +0530

    Add BARBARIK offline service worker

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='barbarik-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./index.html')))));
